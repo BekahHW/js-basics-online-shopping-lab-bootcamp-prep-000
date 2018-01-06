67c92bbe11a572f2eade6137525f44b7f95e2623
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-01-06 20:23:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ else if (cart.length===2){console.log(`In your cart, you have ${Object.keys(cart
 
 else if (cart.length ===3){console.log(`In your cart, you have ${Object.keys(cart[0])} at $${cart[0][Object.keys(cart[0])]}, ${Object.keys(cart[1])} at $${cart[1][Object.keys(cart[1])]}, and ${Object.keys(cart[2])} at $${cart[2][Object.keys(cart[2])]}.`)}
 
- else if (cart.length =>4){console.log(`In your cart, you have ${Object.keys(cart[0])} at $${cart[0][Object.keys(cart[0])]}, ${Object.keys(cart[1])} at $${cart[1][Object.keys(cart[1])]}, and ${Object.keys(cart[2])} at $${cart[2][Object.keys(cart[2])]}, and ${Object.keys(cart[item.length+1])}at $${cart[item.length+1])]}.`}
+ else if (cart.length+1){console.log(`In your cart, you have ${Object.keys(cart[0])} at $${cart[0][Object.keys(cart[0])]}, ${Object.keys(cart[1])} at $${cart[1][Object.keys(cart[1])]}, and ${Object.keys(cart[2])} at $${cart[2][Object.keys(cart[2])]}, and ${Object.keys(cart[item.length+1])}at $${cart[item.length+1])]}.`}
 
 
   
