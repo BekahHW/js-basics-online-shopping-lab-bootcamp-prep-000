8ecb274544d7691e67e48cbd1f58a9a5c620afa0
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-01-06 19:53:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -42,9 +42,11 @@ function total() {
 function removeFromCart(item) {
     for (var i = 0; i<cart.length; i++)
     
-  if (cart[i].hasOwnProperty){console.log(cart[i].slice(i))}
+  if (cart[i].hasOwnProperty);
+  cart = cart[i].slice(i+1);
   
-  return cart
+  return 
+  else {console.log('That item is not in your cart.')} 
   
   // write your code here
 }
