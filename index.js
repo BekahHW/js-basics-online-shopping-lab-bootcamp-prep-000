d912dcaa76828ff2e3d3cb7afb901ed79593860b
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-01-06 21:11:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,11 +23,11 @@ else if (cart.length === 1){console.log(`In your cart, you have ${Object.keys(ca
 else if (cart.length===2){console.log(`In your cart, you have ${Object.keys(cart[0])} at $${cart[0][Object.keys(cart[0])]} and ${Object.keys(cart[1])} at $${cart[1][Object.keys(cart[1])]}.`)}
 
 else {
-  let itemArr = [];
+  let itemStr = [];
      for (let i = 0; i < cart.length ; i++){
        
      
-      itemArr.push(`${Object.keys(cart[i])} at $${cart[i][Object.keys(cart[i])]}`);
+      itemStr = itemStr + `${i === cart.length - 1 && 'and '}${Object.keys(cart[i])} at $${cart[i][Object.keys(cart[i])]}${i < cart.length - 1 ? ', ': '.'}`;
      }
        console.log(`In your cart, you have ${itemArr.join(', ')}.`);
 }
