c7d30a9412f38e83ca95d2ae246e39a6d5af58cc
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-01-06 19:10:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,21 +10,47 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+    cart.push({[item]: Math.floor(Math.random()*100)})
+    console.log(`${item} has been added to your cart.`)
+    return cart
 }
 
 function viewCart() {
-  // write your code here
+if (cart.length === 0){console.log('Your shopping cart is empty.')}
+else if (cart.length === 1){`In your cart, you have ${Object.keys(cart[0])} at $${cart[0][Object.keys(cart[0])]}.`}
+
+else if (cart.length===2){`In your cart, you have ${Object.keys(cart[0])} at $${cart[0][Object.keys(cart[0])]} and ${Object.keys(cart[1])} at $${cart[1][Object.keys(cart[1])]}.`}
+
+else if (cart.length ===3){`In your cart, you have ${Object.keys(cart[0])} at $${cart[0][Object.keys(cart[0])]}, ${Object.keys(cart[1])} at $${cart[1][Object.keys(cart[1])]}, and ${Object.keys(cart[2])} at $${cart[2][Object.keys(cart[2])]}.`}
+  
+
+
+
+
+ 
 }
 
 function total() {
-  // write your code here
+ 
+ var tot = 0; 
+ for (var i = 0; i <cart.length; i++){
+ tot += cart[i][Object.keys(cart[i])]}
+ return tot;
+
 }
 
 function removeFromCart(item) {
+    for (var i = 0; i<cart.length; i++)
+  if (false){console.log('That item is not in your cart.')} else{console.log(delete cart[i].item)}
+  
+  return cart
+  
   // write your code here
 }
 
 function placeOrder(cardNumber) {
+  if (false){console.log("Sorry, we don\'t have a creditcard on file for you.")} 
+  
+  else{console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)}
   // write your code here
 }
