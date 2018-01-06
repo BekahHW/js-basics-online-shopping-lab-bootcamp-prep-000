3e7af9e0cde7776e26fe0a361de3d06e7c4e2a19
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-01-06 21:20:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ if (cart.length === 0){console.log('Your shopping cart is empty.')}
 
 else if (cart.length === 1){console.log(`In your cart, you have ${Object.keys(cart[0])} at $${cart[0][Object.keys(cart[0])]}.`)}
 
-// else if (cart.length===2){console.log(`In your cart, you have ${Object.keys(cart[0])} at $${cart[0][Object.keys(cart[0])]} and ${Object.keys(cart[1])} at $${cart[1][Object.keys(cart[1])]}.`)}
+else if (cart.length===2){console.log(`In your cart, you have ${Object.keys(cart[0])} at $${cart[0][Object.keys(cart[0])]} and ${Object.keys(cart[1])} at $${cart[1][Object.keys(cart[1])]}.`)}
 
 else {
   // Create empty string var to house items and prices
