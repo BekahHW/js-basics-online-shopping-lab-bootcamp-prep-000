93c3dfd29821a3d09370435cdf76e21c6f09dae1
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-01-06 19:32:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (false){console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)}
+  if (true){console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)}
   cart= []}
   else {console.log("Sorry, we don\'t have a credit card on file for you.")} 
   
