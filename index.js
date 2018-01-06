ce6d07132f94a6d3bf1b36ae0848d23c07067fb4
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-01-06 20:26:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,13 @@ else if (cart.length === 1){console.log(`In your cart, you have ${Object.keys(ca
 
 else if (cart.length===2){console.log(`In your cart, you have ${Object.keys(cart[0])} at $${cart[0][Object.keys(cart[0])]} and ${Object.keys(cart[1])} at $${cart[1][Object.keys(cart[1])]}.`)}
 
-else if (cart.length >=3){console.log(`In your cart, you have ${Object.keys(cart[0])} at $${cart[0][Object.keys(cart[0])]}, ${Object.keys(cart[1])} at $${cart[1][Object.keys(cart[1])]}, and ${Object.keys(cart[2])} at $${cart[2][Object.keys(cart[2])]}.`)}
+else {
+     for (let i = 0; i < cart.length ; i++)
+       if (i !== cart.length-1)
+         printValue += `${Object.keys(cart[i])} at $${cart[i][Object.keys(cart[i])]}, `
+       else
+         printValue += `and ${Object.keys(cart[i])} at $${cart[i][Object.keys(cart[i])]}.`
+     console.log(printValue);
 
 }
 
