4dd955574ef6813a5dc5e3ec88dbb951d3c0b4b7
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-01-06 19:57:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ function removeFromCart(item) {
     for (var i = 0; i<cart.length; i++)
     
   if (cart[i].hasOwnProperty(item));
-  cart = cart.slice(0, i).concat.slice();
+  cart = cart.slice(0, i).concat(cart.slice(i+1));
   {console.log(cart)}
   
  
