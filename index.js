2650494588a36badae0a84a607fe63b4bdc60de0
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-01-06 19:40:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ function total() {
 function removeFromCart(item) {
     for (var i = 0; i<cart.length; i++)
     
-  if (!item){console.log('That item is not in your cart.')} else{console.log(delete cart[i].item)}
+  if (!item){console.log('That item is not in your cart.')} else{console.log(item.splice())}
   
   return cart
   
